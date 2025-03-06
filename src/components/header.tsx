e0d58d3b83a8378c38debd960fd18b7c82ea9f2d
Anderson T <tseng.andersonn@gmail.com>
2025-03-06 04:09:41 -0800

    clean up styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,13 +6,13 @@ const Header = () => {
             <nav className="w-full mx-auto flex items-center justify-between ">
             
             <div className="justify-start">
-                <Link href="/" className="text-xl font-bold text-gray-500">
+                <Link href="/" className="text-xl font-bold text-gray-500 dark:hover:text-gray-200 hover:text-gray-900">
                 <span className="min-[300px]:hidden">AT</span>
                 <span className="hidden min-[300px]:inline">Anderson Tseng</span>
                 </Link>
             </div>
 
-            <div className="flex sm:gap-4 justify-end grow max-[99px]:w-full">
+            <div className="flex sm:gap-4 justify-end grow max-[99px]:w-full text-lg font-bold">
                 <Link href="/about" className="p-2 text-gray-500 dark:hover:text-gray-200 hover:text-gray-900">
                 About
                 </Link>
