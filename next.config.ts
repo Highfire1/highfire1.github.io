d1b83a5e48161b264ea23f1ee217575e333a671e
Anderson T <tseng.andersonn@gmail.com>
2025-03-26 12:42:39 -0700

    update readme

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+// note that the github pages workflow overwrites next.config
+// so any values here are only for development
+
 const nextConfig: NextConfig = {
-  output: "export",
+  // output: "export",
+  
   // disabled because:
   // a) i dislike the optimized links that next.js generates
   // b) it doesn't work with github pages
