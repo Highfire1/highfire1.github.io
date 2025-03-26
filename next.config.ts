27044725eb0718f8f338de32662fa589c1c14f7d
Anderson T <tseng.andersonn@gmail.com>
2025-03-25 17:53:45 -0700

    cleanup

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
-// const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
-
 const nextConfig: NextConfig = {
   output: "export",
-  // ...(isGithubActions && {
-  //   output: "export"
-  // }),
-  // images: {
-  //   unoptimized: true
-  // }
-  /* config options here */
 };
 
 export default nextConfig;
